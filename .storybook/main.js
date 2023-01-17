69611375721c39dd0c2e06e8f8d979f679dab696
Germain <germain@souquet.com>
2023-01-17 14:01:55 +0000

    Add typography files to documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-addon-designs"
   ],
   "staticDirs": ['../public'],
   "framework": "@storybook/react"
