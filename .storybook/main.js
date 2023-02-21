ead7cd2b2f82ef0464afa9af878d63cce04caef2
Germain <germain@souquet.com>
2023-02-21 13:00:10 +0000

    First storybook integration

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,11 @@ const ElementTheme =  require('./ElementTheme');
 
 module.exports = {
   "stories": [
-    "../docs/**/*.stories.mdx",
-    "../docs/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.md",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    "@storybook/addon-docs",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
