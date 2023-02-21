920a761ae9312fe5ffbe504120ce1b93644c3ecd
Germain <germain@souquet.com>
2023-02-21 13:41:38 +0000

    upgrade sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,13 @@
-const ElementTheme =  require('./ElementTheme');
-
+const ElementTheme = require('./ElementTheme');
 module.exports = {
-  "stories": [
-    "../docs/**/*.stories.mdx",
-    "../docs/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-addon-designs"
-  ],
+  "stories": ["../docs/**/*.mdx", "../docs/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-designs"],
   "staticDirs": ['../public'],
-  "framework": "@storybook/react"
-}
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
