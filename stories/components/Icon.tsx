c7a28cf4c746a9ba6f785a754cb756d19cd2cfe6
Germain <germains@element.io>
2023-05-02 09:32:29 +0100

    Compound icons

--- stories/components/Icon.tsx
+++ stories/components/Icon.tsx
@@ -1,33 +1,47 @@
 import React from "react";
 
+function chunkArray(arr) {
+  const chunkSize = 7;
+  const numChunks = Math.ceil(arr.length / chunkSize);
+  return new Array(numChunks).fill(0).map((_, index) => {
+    return arr.slice(index * chunkSize, (index + 1) * chunkSize);
+  });
+}
+
 export default function ({ tokens }) {
   const icons = Object.keys(tokens.icon);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Icon</th>
-        </tr>
-      </thead>
-      <tbody>
-        {icons.map((iconName) => (
-          <tr key={iconName}>
-            <td>{iconName}</td>
-            <td>
-              <div
-                style={{
-                  width: "40px",
-                  height: "40px",
-                  background: `var(--cpd-icon-${iconName})`,
-                  backgroundSize: "contain",
-                }}
-              />
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <h1>Compound icons</h1>
+      <table
+        style={{
+          width: "min(90%, 920px)",
+          textAlign: "center",
+        }}
+      >
+        <tbody>
+          {chunkArray(icons).map((chunk, index) => (
+            <tr key={`chunk-${index}`}>
+              {chunk.map((iconName) => (
+                <td key={iconName}>
+                  {iconName}
+                  <br />
+                  <span
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      display: "inline-block",
+                      background: `var(--cpd-icon-${iconName})`,
+                      backgroundSize: "contain",
+                    }}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
