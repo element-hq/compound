6eed85c9130ba99d1c8ef65a1b3f0588ba86255c
Germain <germains@element.io>
2023-05-11 08:41:06 +0100

    move typography component to storybook

--- stories/Styles.stories.jsx
+++ stories/Styles.stories.jsx
@@ -4,17 +4,15 @@ import tokens from "@vector-im/compound-design-tokens";
 import IconsTokens from "@vector-im/compound-design-tokens/icons/$icons.json";
 
 import SpacingComponent from "./components/Spacing";
-import TypographyComponent from "./components/Typography";
 import ColorComponent from "./components/Color";
 import IconComponent from "./components/Icon";
 
 export default {
   title: 'Styles/Styles',
-  component: SpacingComponent | TypographyComponent | ColorComponent | IconComponent,
+  component: SpacingComponent |  ColorComponent | IconComponent,
 };
 
 export const Spacing = () => <SpacingComponent tokens={tokens.light} />;
-export const Typography = () => <TypographyComponent tokens={tokens.light} />;
 export const Color = () => <ColorComponent tokens={tokens} />;
 export const Icon = () => <IconComponent tokens={IconsTokens} />;
 
