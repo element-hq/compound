fc89afa7c5c659852646eda2d9160c8c75bd9714
Germain <germains@element.io>
2023-05-11 09:27:29 +0100

    Move Spacing to tokens section

--- stories/Styles.stories.jsx
+++ stories/Styles.stories.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import tokens from "@vector-im/compound-design-tokens";
 
 import IconsTokens from "@vector-im/compound-design-tokens/icons/$icons.json";
 
-import SpacingComponent from "./components/Spacing";
 import IconComponent from "./components/Icon";
 
 export default {
   title: 'Styles/Styles',
-  component: SpacingComponent | IconComponent,
+  component:  IconComponent,
 };
 
-export const Spacing = () => <SpacingComponent tokens={tokens.light} />;
 export const Icon = () => <IconComponent tokens={IconsTokens} />;
-
-
-
