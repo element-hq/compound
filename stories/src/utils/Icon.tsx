691db3afa0a29549fd5dd9ab7e4462a34a9b6ac2
Robin <robin@robin.town>
2024-02-01 16:22:47 -0500

    Build with Vite
    Vite is a significantly faster builder than Webpack which the JS ecosystem seems to be adopting widely. We're already using for Compound Web, and Storybook makes it pretty simple to swap it out for Webpack, so that's what I've done here.

--- stories/src/utils/Icon.tsx
+++ stories/src/utils/Icon.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const Icon: React.FC<{iconName: string}> = ({ iconName }) => {
-    return <div className="cpdIconItem">
-        <img className="cpdIcon" alt={iconName} src={require(`@vector-im/compound-design-tokens/icons/${iconName}.svg`)}/>
-        <p>{iconName}</p>
+const icons = import.meta.glob(
+  `../../../node_modules/@vector-im/compound-design-tokens/icons/*.svg`
+);
+
+export const Icon: React.FC<{ iconName: string }> = ({ iconName }) => {
+  const [src, setSrc] = useState<string | undefined>(undefined);
+  useEffect(() => {
+    icons[
+      `../../../node_modules/@vector-im/compound-design-tokens/icons/${iconName}.svg`
+    ]().then((src) => setSrc((src as { default: string }).default));
+  }, [iconName]);
+
+  return (
+    <div className="cpdIconItem">
+      <img className="cpdIcon" alt={iconName} src={src} />
+      <p>{iconName}</p>
     </div>
-}
+  );
+};
